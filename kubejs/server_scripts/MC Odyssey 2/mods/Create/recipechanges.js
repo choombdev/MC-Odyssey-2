945128a5ba60e96f43fd55896f71594496f9f18b
Choomb <Yahiadalash@gmail.com>
2022-09-14 17:49:31 +0200

    Update V2.3

---
+++ kubejs/server_scripts/MC Odyssey 2/mods/Create/recipechanges.js
@@ -0,0 +1,5 @@
+// This script was made by choombdev for Odyssey 2, reusing it without explicit permission is not allowed.
+onEvent('recipes', event => {
+    // Remove the recipe for Netherrack Crushing in Create
+    event.remove({id:"create:kjs_13ebmow2l68rxnxj4fuvibkyy"})
+})
